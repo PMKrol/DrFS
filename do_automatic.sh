22b5e839098c817da34780d61f4616c8f91cc419
Patryk M Król <88658026+PMKrol@users.noreply.github.com>
2025-04-01 11:14:13 +0200

    Delete do_automatic.sh

--- do_automatic.sh
+++
@@ -1,5 +0,0 @@
- chmod +x make_bin.sh && ./make_bin.sh 
- ./cut 707,0,2848,2848 data/*/*/
- chmod +x do_align.sh && ./do_align.sh data/*/*/
- ./stack -m11 -a12 data/*/*/
- ./findEdges data/*/*/wip/Stack-m11.Aligned-a12.png
